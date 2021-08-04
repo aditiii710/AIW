8e57bee1be8e274fd89c60f72e1437d677d23353
Abhinav Mishra <69217002+Avi-code-byte@users.noreply.github.com>
2021-08-04 21:52:47 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const experss = require("express");
 const router = experss.Router();
 
 router.get("/", (req, res) => res.render("home"));
+router.get("/contact", (req, res) => res.render("contact"));
 
 module.exports = router;
